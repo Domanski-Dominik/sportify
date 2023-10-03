100083c2615f41a0fbaf0825dc1a117c1b85f7df
Domanski-Dominik <domanbuty@gmail.com>
2023-10-03 12:56:34 +0200

    pwa no scaling2

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="pl">
-       
+       <head>
           <meta name="apple-mobile-web-app-capable" content="yes"/>
           <meta name="mobile-web-app-capable" content="yes"/>
           <meta name="apple-mobile-web-app-status-bar-style" content="black"></meta>
@@ -21,7 +21,7 @@ const RootLayout = ({ children }) => {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-       
+       </head>
         <body className=''>
         <Provider>
             <Nav />
